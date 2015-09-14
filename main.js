f33e6a5326cd7832abdd14a028484f592d1fb9cd
Jozef Butko <j.butko@danube-soft.sk>
2015-09-14 12:59:57 +0200

    Init commit

--- main.js
+++ main.js
@@ -9,66 +9,81 @@ var ipc = require('ipc');
 var mainWindow = null;
 var settingsWindow = null;
 
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
 app.on('ready', function() {
-    if (!configuration.readSettings('shortcutKeys')) {
-        configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
-    }
+  if (!configuration.readSettings('shortcutKeys')) {
+    configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+  }
 
-    mainWindow = new BrowserWindow({
-        frame: false,
-        height: 700,
-        resizable: false,
-        width: 368
-    });
+  mainWindow = new BrowserWindow({
+    frame: true,
+    height: 700,
+    resizable: true,
+    width: 368
+  });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+  // jQuery referrence error loading bug fix
+  mainWindow.webContents.on('did-start-loading', function() {
+    mainWindow.webContents.executeJavaScript("var $ = jQuery = require('jquery'), mainWindow = require('remote').getCurrentWindow();");
+  });
 
-    setGlobalShortcuts();
+  mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+  // Open the devtool
+  mainWindow.openDevTools();
+
+  setGlobalShortcuts();
 });
 
 function setGlobalShortcuts() {
-    globalShortcut.unregisterAll();
+  globalShortcut.unregisterAll();
 
-    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
-    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+  var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+  var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
 
-    globalShortcut.register(shortcutPrefix + '1', function () {
-        mainWindow.webContents.send('global-shortcut', 0);
-    });
-    globalShortcut.register(shortcutPrefix + '2', function () {
-        mainWindow.webContents.send('global-shortcut', 1);
-    });
+  globalShortcut.register(shortcutPrefix + '1', function() {
+    mainWindow.webContents.send('global-shortcut', 0);
+  });
+  globalShortcut.register(shortcutPrefix + '2', function() {
+    mainWindow.webContents.send('global-shortcut', 1);
+  });
 }
 
-ipc.on('close-main-window', function () {
-    app.quit();
+ipc.on('close-main-window', function() {
+  app.quit();
 });
 
-ipc.on('open-settings-window', function () {
-    if (settingsWindow) {
-        return;
-    }
+ipc.on('open-settings-window', function() {
+  if (settingsWindow) {
+    return;
+  }
 
-    settingsWindow = new BrowserWindow({
-        frame: false,
-        height: 200,
-        resizable: false,
-        width: 200
-    });
+  settingsWindow = new BrowserWindow({
+    frame: false,
+    height: 200,
+    resizable: false,
+    width: 200
+  });
 
-    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
+  settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
 
-    settingsWindow.on('closed', function () {
-        settingsWindow = null;
-    });
+  settingsWindow.on('closed', function() {
+    settingsWindow = null;
+  });
 });
 
-ipc.on('close-settings-window', function () {
-    if (settingsWindow) {
-        settingsWindow.close();
-    }
+ipc.on('close-settings-window', function() {
+  if (settingsWindow) {
+    settingsWindow.close();
+  }
 });
 
-ipc.on('set-global-shortcuts', function () {
-    setGlobalShortcuts();
+ipc.on('set-global-shortcuts', function() {
+  setGlobalShortcuts();
 });
